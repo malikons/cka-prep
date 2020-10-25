8f85739a71191395ba60e50f1053386503f379ea
malikons <shiraz.malik@msn.com>
2020-10-25 11:57:27 +0100

    removed aws keys

--- myfirst.tf
+++ myfirst.tf
@@ -1,11 +1,9 @@
 provider "aws" {
   region  = "eu-west-2"
-  access_key = "AKIAJGIJZWUMLROUEQYA"
-  secret_key = "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"
 }
 
 resource "aws_instance" "my-first-tf-instance" {
-  ami           = ami-05c424d59413a2876
+  ami           = "ami-05c424d59413a2876"
   instance_type = "t2.micro"
 }
 
